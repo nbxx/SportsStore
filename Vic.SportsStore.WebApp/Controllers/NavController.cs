22fd698f385830238ff89343dd2f820e176e3563
yuguan <yuguan>
2018-08-25 18:18:27 -0400

    1 [NEW] add nav menu;

--- Vic.SportsStore.WebApp/Controllers/NavController.cs
+++ Vic.SportsStore.WebApp/Controllers/NavController.cs
@@ -16,8 +16,10 @@ namespace Vic.SportsStore.WebApp.Controllers
             repository = repo;
         }
 
-        public PartialViewResult Menu()
+        public PartialViewResult Menu(string category = null)
         {
+            ViewBag.SelectedCategory = category;
+
             IEnumerable<string> categories = repository
                 .Products
                 .Select(x => x.Category)
